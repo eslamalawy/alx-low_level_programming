ee67741e3e5e7872f370772fe2c1445bf88bd8b2
sadat <eslammo4kala@gmail.com>
2023-04-17 05:02:41 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef dog dog_t;
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
