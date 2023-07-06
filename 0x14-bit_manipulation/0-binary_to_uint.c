343788733aef796309f99c1ab569ebe1c91d2fe4
Eslam Alawy <eslammo4kala@gmail.com>
2023-07-06 02:54:14 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number string to unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the unsigned int form of b
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		num = num * 2 + (*b++ - '0');
+	}
+	return (num);
+}
